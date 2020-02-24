ea6eb6f5f9a5de70d2279b1e28e32878b1ad7365
fahmi zouri <fahmi.zouri@gmail.com>
2020-02-24 14:48:54 +0100

    db commit

--- src/main/java/com/example/demo/ServeurGestionStockProduitApplication.java
+++ src/main/java/com/example/demo/ServeurGestionStockProduitApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.entity.Produit;
+import com.example.demo.repository.ProduitRepository;
 
 @SpringBootApplication
 public class ServeurGestionStockProduitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServeurGestionStockProduitApplication.class, args);
+		ConfigurableApplicationContext ctx =SpringApplication.run(ServeurGestionStockProduitApplication.class, args);
+		ProduitRepository produitRepository=ctx.getBean(ProduitRepository.class);
+		
+		produitRepository.save(new Produit("livre",50,20));
+		produitRepository.save(new Produit("cahier",200,5.25f));
+		produitRepository.save(new Produit("stylo",500,2.10f));
+		
 	}
 
 }
--- src/main/java/com/example/demo/entity/Produit.java
+++ src/main/java/com/example/demo/entity/Produit.java
@@ -1,7 +1,23 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produit {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	private String ref;
 	private int quantite;
 	private float prixunitaire;
@@ -27,11 +43,12 @@ public class Produit {
 	public void setQuantite(int quantite) {
 		this.quantite = quantite;
 	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((ref == null) ? 0 : ref.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 	@Override
@@ -43,10 +60,10 @@ public class Produit {
 		if (getClass() != obj.getClass())
 			return false;
 		Produit other = (Produit) obj;
-		if (ref == null) {
-			if (other.ref != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!ref.equals(other.ref))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
